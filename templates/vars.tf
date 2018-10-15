1d4e62699756fdd0bbc8b40b2a1d28c1a76a2221
tikitavi <rtikitavi@gmail.com>
2018-10-15 18:20:09 +0300

    add security group creation
    add creation of security group with inbound rules that permit access
     to SVL and PL team
    minor fixes:
    - changed default region

--- templates/vars.tf
+++ templates/vars.tf
@@ -1,34 +1,39 @@
 variable "aws_region" {
     description = "The AWS region."
-    default = "us-east-1"
+    default     = "us-east-2"
 }
 
 variable "deployment_name" {
     description = "The name of deployment. Will be the part of resources names."
-    default = "auto"
+    default     = "auto"
 }
 
 variable "vpc_cidr_block" {
     description = " The cidr block of the desired VPC."
-    default = "10.10.0.0/16"
+    default     = "10.10.0.0/16"
 }
 
 variable "vpc_enable_dns" {
     description = "Whether or not the VPC has DNS support."
-    default = "true"
+    default     = "true"
 }
 
 variable "vpc_enable_dns_hostnames" {
     description = "Whether or not the VPC has DNS hostname support."
-    default = "true"
+    default     = "true"
 }
 
 variable "subnet_cidr_block" {
     description = "The cidr block of the desired subnet."
-    default = "10.10.0.0/20"
+    default     = "10.10.0.0/20"
 }
 
 variable "subnet_map_public_ip" {
     description = "Specify true to indicate that instances launched into the subnet should be assigned a public IP address. Default is false."
-    default = "false"
+    default     = "false"
+}
+
+variable "sgroup_inbound_cidr_blocks" {
+    description = "List of CIDR blocks for ingress rule."
+    default     = ["66.129.239.10/31", "66.129.239.12/30", "66.129.239.16/30", "66.129.242.10/31", "66.129.242.12/30", "66.129.242.16/30", "213.208.163.170/32"]
 }
--- templates/vpc.tf
+++ templates/vpc.tf
@@ -45,3 +45,23 @@ resource "aws_route_table_association" "route_table_association" {
     subnet_id      = "${aws_subnet.subnet.id}"
     route_table_id = "${aws_route_table.route_table.id}"
 }
+
+resource "aws_security_group" "default" {
+  vpc_id      = "${aws_vpc.vpc.id}"
+  name        = "default-${var.deployment_name}"
+  vpc_id      = "${aws_vpc.vpc.id}"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["${var.sgroup_inbound_cidr_blocks}"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
